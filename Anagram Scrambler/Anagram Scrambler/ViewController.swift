f3c9688a234c6a64e0238b1519fd9a2781252a7f
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-20 10:35:05 -0600

    Add initial starter template

---
+++ Anagram Scrambler/Anagram Scrambler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Anagram Scrambler
+//
+//  Created by Akshay Ramesh on 12/20/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
