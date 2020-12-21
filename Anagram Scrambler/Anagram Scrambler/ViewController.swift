78268ef24682baad9e9888e27b163c848d63e006
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-20 22:39:02 -0600

    Added uialertcontroller for user input and saving the input in the tableview

--- Anagram Scrambler/Anagram Scrambler/ViewController.swift
+++ Anagram Scrambler/Anagram Scrambler/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptAnswer))
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -41,6 +41,22 @@ class ViewController: UITableViewController {
     func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
+        tableView.reloadData()
+    }
+    
+    @objc func promptAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        usedWords.append(answer)
         tableView.reloadData()
     }
 
