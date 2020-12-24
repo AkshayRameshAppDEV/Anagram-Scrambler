53fa5c5925bb4c56b97874ae62b0db3db9f354b9
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-23 22:54:29 -0600

    disallowing answers if the word is shorter than 3 letters or if it is same as the navigation bar title

--- Anagram Scrambler/Anagram Scrambler/ViewController.swift
+++ Anagram Scrambler/Anagram Scrambler/ViewController.swift
@@ -105,6 +105,9 @@ class ViewController: UITableViewController {
     }
     
     func isReal(word: String) -> Bool {
+        if(word.count <= 3 || word == title) {
+            return false
+        }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
